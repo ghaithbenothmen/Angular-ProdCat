0e32a01412d4fc8eace2e78ae429f3d85c4a32a9
Ben Othmen Ghaith <114808083+ghaithbenothmen@users.noreply.github.com>
2024-10-13 19:37:19 +0100

    query param path param

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './composants/header/header.component';
+import { FooterComponent } from './composants/footer/footer.component';
+import { HomeComponent } from './composants/home/home.component';
+import { ListCategoriesComponent } from './composants/list-categories/list-categories.component';
+import { ProfileComponent } from './modules/profile/profile/profile.component';
+import { FormsModule } from '@angular/forms';
+import { TablevaluePipe } from './pipes/tablevalue.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
+import { NotFoundPageComponent } from './composants/not-found-page/not-found-page.component';
+import { ProductsCategoryComponent } from './composants/products-category/products-category.component';
+import { ProductsCategoryQPComponent } from './composants/products-category-qp/products-category-qp.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListCategoriesComponent,
+    ProfileComponent,
+    TablevaluePipe,
+    HighlightDirective,
+    NotFoundPageComponent,
+    ProductsCategoryComponent,
+    ProductsCategoryQPComponent,
+      
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
