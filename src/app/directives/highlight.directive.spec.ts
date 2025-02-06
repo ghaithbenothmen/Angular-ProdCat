c0510efa3c93eb9d62bacbd572fff31522fd01bb
Ben Othmen Ghaith <114808083+ghaithbenothmen@users.noreply.github.com>
2025-02-06 18:55:31 +0100

    Update highlight.directive.spec.ts

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,11 @@
+import { ElementRef, Renderer2 } from '@angular/core';
 import { HighlightDirective } from './highlight.directive';
 
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    const el = new ElementRef(document.createElement('div'));
+    const renderer = jasmine.createSpyObj('Renderer2', ['setStyle']);
+    const directive = new HighlightDirective(el, renderer);
     expect(directive).toBeTruthy();
   });
 });
